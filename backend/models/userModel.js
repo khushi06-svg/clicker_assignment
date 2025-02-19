b147c3915ee411cdb1ba1f2e031b31f6428a7072
Khushi Pal <65907675+khushi06-svg@users.noreply.github.com>
2025-02-20 03:26:22 +0530

    Delete backend directory

--- backend/models/userModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const userSchema = new mongoose.Schema({
-    username: { type: String, unique: true, required: true },
-    counter: { type: Number, default: 0 },
-    prizes: { type: Number, default: 0 }
-});
-
-export default mongoose.model('User', userSchema);
